daf5171ba70d16074734c1c025ba007d51fb159a
piran777 <paminul@uwo.ca>
2023-12-03 19:04:52 -0500

    Added training and testing splits that may need to be adjusted later

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,29 @@
+import matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import seaborn as sns
+
+
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler
+from sklearn.metrics import make_scorer, mean_absolute_error, r2_score, accuracy_score, precision_score
+
 
 # Assuming the TXT file is tab-separated
 df = pd.read_csv('traffic.csv')
 # Display the first few rows of the DataFrame (To test if its working)
 print(df.head())
+
+#potential pre-processing required
+
+
+#Splitting the data into training and testing splits
+X = df.drop("Traffic Situation" , axis = 'columns').values #Drops target column
+y = df["Traffic Situation"].values #retrieves the target column
+
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.30, random_state = 42)
+#Test_size is 30% and 70% is for training
