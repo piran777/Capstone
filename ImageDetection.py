d6ca678cde31c95a711bce9788caa0863e65fd9b
Martin Todev <mtodev@uwo.ca>
2024-03-06 15:05:42 -0500

    Image Detection for a single image

--- ImageDetection.py
+++ ImageDetection.py
@@ -1,4 +1,6 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("yolov8x.pt")  
+# Load a pretrained YOLO model
+model = YOLO('yolov8n.pt')
+
+results = model(source = "D:\School\Fourth Year ENG\SE 4450 Software Engineering Design\Capstone\Images\Highway Test Image.jpeg", show = True, conf = 0.4, save = True) # Generator of results object
