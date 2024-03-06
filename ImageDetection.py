2f3cd94e12746728ecf4724dd456e059ac38594e
Martin Todev <mtodev@uwo.ca>
2024-03-06 15:52:32 -0500

    Counts amount of each vehicle type. And created csv to track them

--- ImageDetection.py
+++ ImageDetection.py
@@ -10,7 +10,26 @@ model = YOLO('yolov8n.pt')
 
 results = model(source = './Images/Highway Test Image.jpeg', conf = 0.4, save = False) # Generator of results object
 
+carCount = 0
+bikeCount = 0
+busCount = 0
+TruckCount = 0
+none = 0
+
 for box in results[0].boxes:
         class_id = int(box.cls)  # Get class ID
         class_label = results[0].names[class_id]  # Get class label from class ID
-        print(f'Detected class: {class_label}')  # Print class label
+        #print(f'Detected class: {class_label}')  # Print class label
+
+        match class_label:
+                case "car":
+                        carCount += 1
+                case "truck":
+                        TruckCount += 1
+                case "bus":
+                        busCount += 1
+                case "bike":
+                        bikeCount += 1
+                case _:
+                        none += 1
+
