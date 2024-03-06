1e87826dca54c3601548d362537fee12597ee7ff
Martin Todev <mtodev@uwo.ca>
2024-03-06 15:42:03 -0500

    Can extract labels from image detection

--- ImageDetection.py
+++ ImageDetection.py
@@ -1,6 +1,16 @@
+import torch
 from ultralytics import YOLO
+import csv
 
 # Load a pretrained YOLO model
 model = YOLO('yolov8n.pt')
 
-results = model(source = "D:\School\Fourth Year ENG\SE 4450 Software Engineering Design\Capstone\Images\Highway Test Image.jpeg", show = True, conf = 0.4, save = True) # Generator of results object
+# Define a path to a CSV file with images
+# source = '.\ImagePaths.csv'
+
+results = model(source = './Images/Highway Test Image.jpeg', conf = 0.4, save = False) # Generator of results object
+
+for box in results[0].boxes:
+        class_id = int(box.cls)  # Get class ID
+        class_label = results[0].names[class_id]  # Get class label from class ID
+        print(f'Detected class: {class_label}')  # Print class label
